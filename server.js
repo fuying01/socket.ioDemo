45f92e6ac71c36bab2f448df40dd53b671c5ae35
XShadow <fuying_01@163.com>
2017-08-09 10:25:34 +0800

    ADD: 1.增加游戏结束; 2.退出游戏; 3.每个player只能join,create一个游戏; 4.加入错误信息.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ let io = require('socket.io')(8080)
 //app.listen(8080);
 
 io.on('connection', socket=>{
-	//console.log('get connect =>', socket)
+	console.log('get connect =>')
 	//socket.emit('news', {msg: 'hello'});
 	//socket.on('message', msg=>{
 	//	console.log(msg);
@@ -14,9 +14,20 @@ io.on('connection', socket=>{
 
 gameRoom = []
 
+let root = io.of('/').on('connection', socket=>{
+	console.log('of connection: /');
+	socket.on('message', msg=>{
+		console.log('receive => ', msg);
+	});
+})
+
 let game = io.of('/game').on('connection', socket=>{
 	console.log('of /game');
 	socket.on('create', data=>{
+		if (socket.game) {
+			socket.emit('gameError', {errMsg: 'Create game error. Your have join the game' + socket.gameId})
+			return;
+		}
 		let gameId = gameRoom.length;
 		console.log('create game =>', gameId);
 		gameRoom.push({
@@ -26,11 +37,34 @@ let game = io.of('/game').on('connection', socket=>{
 				name: data.name
 			}]
 		})
-		socket.emit('create', {content: 'create room success: ', gameId});
+		socket.emit('create', {content: 'create room success: ' + gameId, gameId});
+		socket.gameId = gameId;
+		socket.game = gameRoom[gameId];
 	});
+	socket.on('findGame', data=>{
+		if (socket.game) {
+			socket.emit('gameError', {errMsg: 'Find game Error. You have join the game ' + socket.gameId});
+			return
+		}
+		let res = gameRoom.map( (game, index) => {
+			if (game.disable) return null;
+			return index
+		}).filter( item => item!=null )
+		socket.emit('findGame', {data: 'You can join the game: ' + res.join(', ')})
+	})
 	socket.on('join', data=>{
+		if (socket.game) {
+			socket.emit('gameError', {errMsg: 'You have join the game ' + socket.gameId})
+			return
+		}
 		const { gameId, name } = data;
-		if (!gameRoom[gameId]) socket.emit('gameError', {errMsg: 'Error'});
+		if (!gameRoom[gameId] || gameRoom[gameId].disable) { 
+			socket.emit('gameError', {errMsg: 'Error'});
+			return
+		}
+		
+		socket.gameId = gameId;
+		socket.game = gameRoom[gameId]
 		gameRoom[gameId].player.push({
 			client: socket,
 			name: name
@@ -42,6 +76,7 @@ let game = io.of('/game').on('connection', socket=>{
 		const {playerId, gameId} = data;
 		quitGame(gameRoom[gameId], playerId)
 	})
+	
 })
 
 startGame = (game) => {
@@ -53,13 +88,28 @@ startGame = (game) => {
 	})
 }
 quitGame = (game, index) => {
-	console.log(`=>>>>player:${index}-${game.player[index].name}-quit game.`)
+	console.log(`=>>>>player:${index}-${game.player[index].name}-quit game.`);
 	game.player.forEach( (player, idx) => {
 		if (idx == index) return;
-		player.client.emit('playerQuit', {
+		if (player) player.client.emit('playerQuit', {
 			quitPlayer: {id: index, name: game.player[index].name}
 		})
 	})
+	delete game.player[index].client.game;
+	delete game.player[index].client.gameId;
+	game.player[index] = null
+	stopGame(game, index);
+}
+stopGame = (game, index) => {
+	console.log(' stop game => ', index);
+	game.player.forEach( player => {
+		if (player) {
+			player.client.emit('gameOver');
+			delete player.client.game;
+			delete player.client.gameId;
+		}
+	})
+	game.disable = true;
 }
 
 console.log('=======server start======')
